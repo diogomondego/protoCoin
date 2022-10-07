e8290f1b035b8ef8f14e87c3d926af3129d118f1
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2022-10-06 23:28:40 -0300

    feat: home - fetch coins and create flatList

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,70 @@
+import { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
 import Header from "../../components/Header";
-import { Container } from "./styles";
+import { useCoin } from "../../hooks/coin";
+import api from "../../services/api";
+import colors from "../../utils/colors";
+import { CoinData } from "../../utils/types";
+import { Container, Content, Description, Title, LoadingView, ContentHeader } from "./styles";
 
 export default function Home() {
+  const { coins, setCoins } = useCoin()
+  const [loading, setLoading] = useState(false)
+
+  // Renderizar lista de coins
+  function renderList(item: CoinData) {
+    return (
+      <Description>{item.symbol}</Description>
+    )
+  }
+
+  // Renderizar lista vazia de coins
+  function loadingList() {
+    return (
+      loading &&
+        <LoadingView>
+          <ActivityIndicator color={colors.gray_200} size='large' />
+          <Title>Loading coins...</Title>
+        </LoadingView>
+        
+    )
+  }
+
+  const fetchCoins = useCallback(async () => {
+    try {
+      setLoading(true)
+      const { data } = await api.get('/v3/ticker/24hr')
+
+      setCoins(data)
+    } catch (error) {
+
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <Container>
       <Header />
+      <Content>
+        <ContentHeader>
+          <Title>Nosso Catálogo</Title>
+          <Description>Lista de Criptomoedas disponíveis para você</Description>
+        </ContentHeader>
+        <FlatList
+          data={coins}
+          keyExtractor={item => item.symbol}
+          renderItem={({ item }) => renderList(item)}
+          ListEmptyComponent={loadingList}
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        />
+      </Content>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,6 +3,29 @@ import colors from "../../utils/colors";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
   background-color: ${colors.background};
+`;
+
+export const Content = styled.View`
+  padding: 0 20px;
+  flex: 1;
+`;
+
+export const ContentHeader = styled.View`
+  margin: 20px 0;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: ${colors.text};
+`;
+
+export const Description = styled(Title)`
+  font-size: 14px;
+`
+
+export const LoadingView = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 `;
